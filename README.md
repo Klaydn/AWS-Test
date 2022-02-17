e4e30f96ddcd37925b26f8c77818fc0e4fbc3d05
Jose Ballesteros <Klaydn@users.noreply.github.com>
2022-02-17 12:33:26 -0600

    Files Added
    Adding files to test.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AWS-Test
 AWS Test Site
+
+Basically just files used to test AWS Website Configuration and Git.
+
+Sal-Git if you see this I expect a DM from you telling me you saw this.
+
---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AWS Test Site</title>
+</head>
+<body>
+<?php
+echo("Hello, World!");
+?>
+</body>
+</html>
